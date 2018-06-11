2901cfe50799198f8a0822a82ed58ec200072575
Anton Stagge <antonstagge95@gmail.com>
2018-06-11 12:50:54 +0200

    Initial commit

---
+++ ClientApp/components/Churches.tsx
@@ -0,0 +1,61 @@
+﻿import * as React from 'react';
+import { connect } from 'react-redux';
+import { RouteComponentProps } from 'react-router-dom';
+import * as ChurchStore from '../store/ChurchList';
+import { ApplicationState } from '../store';
+
+type ChurchesProps = ChurchStore.ChurchState
+    & typeof ChurchStore.actionCreators
+    & RouteComponentProps<{}>;
+
+class Churches extends React.Component<ChurchesProps, {}> {
+    componentDidMount() {
+        console.log("did mount");
+        if (this.props.churches.length == 0) {
+            this.props.fetchChurchesBegin();
+            // do the fetch and then
+            this.delay(1000).then(() => {
+                this.props.fetchChurchesSuccess([]);
+            });
+        }
+    }
+
+    delay(ms: number) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    renderChurch(current: ChurchStore.Church) {
+        return <li key={current.telephoneNr}>
+            <ul>
+                <li> name: {current.name} </li>
+                <li> location: {current.location} </li>
+                <li> phone#: {current.telephoneNr} </li>
+            </ul>
+        </li>
+    }
+
+    load() {
+        if (this.props.loading) {
+            return <div> loading... </div>
+        } else {
+            return <div></div>
+        }
+    }
+    public render() {
+        let load = this.load();
+        let currentChurches = this.props.churches.map(this.renderChurch);
+        return <div>
+            <h1> testing </h1>
+            {load}
+            <ul>
+                {currentChurches}
+            </ul>
+        </div>;
+    }
+}
+
+// Wire up the React component to Redux
+export default connect(
+    (state: ApplicationState) => state.churches, // This is what part of the store is mapped to props
+    ChurchStore.actionCreators
+)(Churches) as typeof Churches;
---
+++ ClientApp/store/ChurchList.ts
@@ -0,0 +1,93 @@
+﻿import { Action, Reducer } from 'redux';
+import { AppThunkAction } from './';
+
+// -----------------
+// STATE - This defines the type of data maintained in the Redux store.
+
+export interface ChurchState {
+    churches: Church[];
+    activeChurch: number;
+    loading: boolean;
+    error: any;
+}
+
+export interface Church {
+    name: string;
+    location: string;
+    telephoneNr: number;
+}
+
+// -----------------
+// ACTIONS - These are serializable (hence replayable) descriptions of state transitions.
+// They do not themselves have any side-effects; they just describe something that is going to happen.
+// Use @typeName and isActionType for type detection that works even after serialization/deserialization.
+
+interface SetActiveChurchAction {
+    type: 'SET_ACTIVE_CHURCH';
+    payload: number;
+}
+
+interface RemoveActiveChurch {
+    type: 'REMOVE_ACTIVE_CHURCH';
+}
+
+interface AddChurchesBegin {
+    type: 'ADD_CHURCHES_BEGIN';
+}
+
+interface AddChurchesSuccess {
+    type: 'ADD_CHURCHES_SUCCESS';
+    payload: Church[];
+}
+
+interface AddChurchesError {
+    type: 'ADD_CHURCHES_ERROR';
+    payload: any;
+}
+
+// Declare a 'discriminated union' type. This guarantees that all references to 'type' properties contain one of the
+// declared type strings (and not any other arbitrary string).
+type KnownAction = SetActiveChurchAction | RemoveActiveChurch | AddChurchesBegin | AddChurchesSuccess | AddChurchesError;
+
+// ----------------
+// ACTION CREATORS - These are functions exposed to UI components that will trigger a state transition.
+// They don't directly mutate state, but they can have external side-effects (such as loading data).
+
+export const actionCreators = {
+    setActiveChurch: (payload: number) => <SetActiveChurchAction>{ type: 'SET_ACTIVE_CHURCH', payload: payload },
+    removeActiveChurch: () => <RemoveActiveChurch>{ type: 'REMOVE_ACTIVE_CHURCH' },
+    fetchChurchesBegin: () => <AddChurchesBegin>{type: 'ADD_CHURCHES_BEGIN'},
+    fetchChurchesSuccess: (payload: Church[]) => {
+        let tempChurch: Church = { name: 'Antons Church', telephoneNr: 1337, location: 'stockholm' };
+        return <AddChurchesSuccess>{ type: 'ADD_CHURCHES_SUCCESS', payload: [tempChurch] };
+    },
+    fetchChurchesError: (payload: any) => <AddChurchesError>{type:'ADD_CHURCHES_ERROR', payload: payload}
+};
+
+// ----------------
+// REDUCER - For a given state and action, returns the new state. To support time travel, this must not mutate the old state.
+
+const unloadedState: ChurchState = { churches: [], activeChurch: -1, loading: false, error: null};
+
+export const reducer: Reducer<ChurchState> = (state: ChurchState, incomingAction: KnownAction) => {
+    const action = incomingAction as KnownAction;
+    switch (action.type) {
+        case 'SET_ACTIVE_CHURCH':
+            return { ...state, churches: state.churches, activeChurch: action.payload };
+        case 'REMOVE_ACTIVE_CHURCH':
+            return { ...state, churches: state.churches, activeChurch: 0 };
+        case 'ADD_CHURCHES_BEGIN':
+            return { ...state, loading: true };
+        case 'ADD_CHURCHES_SUCCESS':
+            return { ...state, loading: false, churches: action.payload };
+        case 'ADD_CHURCHES_ERROR':
+            return { ...state, loading: false, error: action.payload };
+        default:
+            // The following line guarantees that every action in the KnownAction union has been covered by a case above
+            const exhaustiveCheck: never = action;
+    }
+
+    // For unrecognized actions (or in cases where actions have no effect), must return the existing state
+    //  (or default initial state if none was supplied)
+    return state || unloadedState;
+};
